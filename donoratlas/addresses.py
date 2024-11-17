b7f06f45523f98151bb005637f04d36dbbc9ec44
Aaron Berger <aaronzberger@gmail.com>
2024-11-17 20:13:08 +0000

    Improve address zip comparison

--- donoratlas/addresses.py
+++ donoratlas/addresses.py
@@ -1,3 +1,4 @@
+import re
 from typing import Optional
 from scourgify import NormalizeAddress
 from pydantic import BaseModel
@@ -22,6 +23,18 @@ def parse_address(address: str) -> PostalAddress:
             setattr(ret, field, value)
     return ret
 
+
+def format_zip(zip: str) -> str:
+    just_numbers = re.sub(r"[^0-9]", "", zip)
+    match len(just_numbers):
+        case 5 | 9:
+            return just_numbers
+        case 4:
+            return "0" + just_numbers
+        case 3:
+            return "00" + just_numbers
+        case _:
+            return just_numbers
 
 def address_similarity(address1: str, address2: str) -> float:
     """
@@ -51,7 +64,20 @@ def address_similarity(address1: str, address2: str) -> float:
     road_score = None if parsed_address1.road is None or parsed_address2.road is None else ((fuzz.WRatio(parsed_address1.road, parsed_address2.road) / 100) * 2 - 1)
     city_score = None if parsed_address1.city is None or parsed_address2.city is None else ((fuzz.WRatio(parsed_address1.city, parsed_address2.city) / 100) * 2 - 1)
     state_score = None if parsed_address1.state is None or parsed_address2.state is None else (1 if parsed_address1.state == parsed_address2.state else -1)
-    zip_score = None if parsed_address1.postcode is None or parsed_address2.postcode is None else ((fuzz.ratio(parsed_address1.postcode, parsed_address2.postcode) / 100)**2 * 2 - 1)
+
+    if parsed_address1.postcode is not None and parsed_address2.postcode is not None:
+        zip1_formatted = format_zip(parsed_address1.postcode)
+        zip2_formatted = format_zip(parsed_address2.postcode)
+        if len(zip1_formatted) == len(zip2_formatted):
+            zip_formatted_score = (fuzz.ratio(zip1_formatted, zip2_formatted) / 100)**2 * 2 - 1
+        else:
+            zip_formatted_score = (fuzz.ratio(zip1_formatted[:5], zip2_formatted[:5]) / 100)**2 * 2 - 1
+    else:
+        zip_formatted_score = None
+
+    zip_raw_score = None if parsed_address1.postcode is None or parsed_address2.postcode is None else ((fuzz.ratio(parsed_address1.postcode, parsed_address2.postcode) / 100)**2 * 2 - 1)
+
+    zip_score = max(zip_formatted_score or 0, zip_raw_score or 0)
 
     to_count = [x for x in [house_number_score, road_score, city_score, state_score, zip_score] if x is not None]
 
